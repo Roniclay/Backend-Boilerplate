ed7003ad14e51c1c262ef0a0d7529ce4f4b4a711
ultr4 <cl.rch09@icloud.com>
2023-12-23 14:42:27 -0300

    :recycle: refactor: Removed wrong property

--- src/fornecedor/dto/create-fornecedor.dto.ts
+++ src/fornecedor/dto/create-fornecedor.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateFornecedorDto {
   @IsNotEmpty()
@@ -16,8 +16,4 @@ export class CreateFornecedorDto {
   @IsString()
   @IsNotEmpty()
   email: string;
-
-  @IsNotEmpty()
-  @IsNumber()
-  userId: number;
 }
